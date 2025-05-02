79854e3f566408ad2b877c1bdae18fbec1d2f54c
archiquito <archiquito@gmail.com>
2025-05-02 17:34:03 -0300

    refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { verifyJwt } from '../../middlewares/verifyJwt';
 import { createUsers } from './create.controller';
 import { authenticate } from './authenticate.controller';
 import { profile } from './profile.controller';
+import { refresh } from './refresh.controller';
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', createUsers);
   app.post('/authenticate', authenticate);
+
+  app.patch('/token/refresh', refresh);
 
   /** AUTHENTICATED */
   app.get('/profile', { onRequest: [verifyJwt] }, profile);
